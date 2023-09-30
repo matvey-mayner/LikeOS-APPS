3b3b61bc6e76f03b1fcd23a83cbd83791a8ac691
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2023-09-30 20:17:13 +0300

    Update Main.lua

--- G2345D.app/Main.lua
+++ G2345D.app/Main.lua
@@ -1,1 +1,5 @@
-loadstring(game:GetService("HttpService"):GetAsync("dth",true))()
+local internet = require("Internet")
+local fs = require("Filesystem")
+
+inet.download("https://raw.githubusercontent.com/matvey-mayner/LikeOS-APPS" .. tostring(file),where)
+download("/bootloader.lua","/system/core/bootloader.lua")
