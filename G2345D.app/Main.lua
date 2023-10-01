cb4915b312d74ba1328ceba02d822a8a8609569c
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2023-10-01 10:00:20 +0300

    Update Main.lua

--- G2345D.app/Main.lua
+++ G2345D.app/Main.lua
@@ -3,3 +3,4 @@ local fs = require("Filesystem")
 
 inet.download("https://raw.githubusercontent.com/matvey-mayner/LikeOS-APPS" .. tostring(file),where)
 download("/bootloader.lua","/system/core/bootloader.lua")
+download("/Fedya.lua","/system/core/Fedya.lua")
