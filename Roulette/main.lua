db0fafe124614e1bc503264079254c58faccaf8a
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-07-09 18:50:23 +0300

    Delete Roulette directory

--- Roulette/main.lua
+++
@@ -1,7 +0,0 @@
-local iowindows = require("iowindows")
-local openbox = require("openbox")
-
------------------------------------------------
-
-local screen = ...
-openbox.runWithSplash(screen, system.getResourcePatch("game.lua"))
